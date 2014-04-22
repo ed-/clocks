bdbc63cf8c61a59d54327afda83e3928fa7af5db
ed- <ed@slumberheart.com>
2014-04-22 00:07:53 -0500

    moved common function to ticktock.js

---
+++ ticktock.js
@@ -0,0 +1,37 @@
+function draw(){
+  var now = new Date();
+  var hours = now.getHours() % 12;
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var milliseconds = now.getMilliseconds();
+
+  var LEDs = document.getElementsByClassName("LED");
+  [].forEach.call(LEDs, function(LED){
+    if (eval(LED.attributes.led.value)){
+      LED.style.opacity = ON;
+    } else {
+      LED.style.opacity = OFF;
+    }
+  });
+
+  var SPINs = document.getElementsByClassName("SPIN");
+  [].forEach.call(SPINs, function(SPIN){
+    var rot = eval(SPIN.attributes.spin.value);
+    var x = eval(SPIN.attributes.spinx.value);
+    var y = eval(SPIN.attributes.spiny.value);
+    var transform = "rotate(" + rot + " " + x + " " + y + ")";
+    SPIN.setAttribute("transform", transform);
+  });
+
+  var pad = '';
+  if (minutes < 10) {
+    pad = '0';
+  }
+
+  document.title = (hours + ':' + pad + minutes);
+}
+
+function start(FPS){
+  var millisecondsPerFrame = 1000.0 / FPS;
+  window.setInterval(draw, millisecondsPerFrame);
+}
